a90bae0fb239974d9e3e06e2d31f5f12d259a657
davidwwang <davidwwang@tencent.com>
2024-10-09 11:50:09 +0800

    add context demo

--- demo/go_demo/testcontext/main.go
+++ demo/go_demo/testcontext/main.go
@@ -9,7 +9,10 @@ import (
 
 func main() {
 	//testCancelContext()
-	testSubCancel()
+	//testSubCancel()
+	//testPassValue()
+	//testConcurrencyControl()
+	testTimeoutControl()
 	fmt.Println("vim-go")
 }
 
@@ -65,4 +68,77 @@ func subFunc(ctx context.Context, name string) {
 		fmt.Println(name, " Done")
 		return
 	}
+}
+
+func testPassValue() {
+	traceID := "12345"
+	ctx := context.WithValue(context.Background(), "traceID", traceID)
+	go processRequest(ctx)
+	time.Sleep(time.Second * 1)
+
+}
+func processRequest(ctx context.Context) {
+	traceID, ok := ctx.Value("traceID").(string)
+	if ok {
+		fmt.Printf("Processing request with traceID: %s\n", traceID)
+	} else {
+		fmt.Println("Processing request without traceID")
+	}
+}
+
+func testConcurrencyControl() {
+	ctx, cancel := context.WithCancel(context.Background())
+	for n := range genFibonacci(ctx) {
+		fmt.Println(n)
+		if n >= 100 {
+			cancel()
+			break
+		}
+	}
+
+}
+
+func genFibonacci(ctx context.Context) <-chan int {
+	c := make(chan int)
+	go func() {
+		defer close(c)
+		a, b := 0, 1
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case c <- a:
+				a, b = b, a+b
+				//time.Sleep(time.Millisecond * 100)
+			}
+		}
+	}()
+	return c
+}
+
+func testTimeoutControl() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+
+	retries := 0
+
+	const maxTryNum = 5
+	retryInterval := 500 * time.Millisecond
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("timeout")
+			return
+		default:
+			retries++
+			if retries > maxTryNum {
+				fmt.Println("Maximum retries reached")
+				return
+			}
+			fmt.Printf("retrying...%d/%d \n", retries, maxTryNum)
+			time.Sleep(retryInterval)
+
+		}
+	}
+
 }
